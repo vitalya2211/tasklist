a7876f9cc8aee809b8f5f4ecf90ab94148a525b8
Vitalya <vitalya.2211@yandex.ru>
2022-08-25 21:41:33 +1000

    try_1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.dataFolder;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<toDoModel> _toDoData;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _toDoData = new BindingList<toDoModel>()
+            {
+                new toDoModel(){Text="test"},
+                new toDoModel(){Text="sumtext"}
+            };
+            toDoLists.ItemsSource = _toDoData;
+        }
+    }
+}
---
+++ dataFolder/toDoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.dataFolder
+{
+    class toDoModel
+    {
+        public DateTime CreateOnData { get; set; } = DateTime.Now;
+        private bool _isDone;
+        private string _text;
+        public bool isDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+        
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+
+        }
+    }
+}
